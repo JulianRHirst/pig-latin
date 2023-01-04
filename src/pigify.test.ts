79ccba421e83207c2242ef1d7798e45c1ffe9331
Julian <mrjrhirst@gmail.com>
2023-01-04 12:15:50 +0000

    Passing tests!

---
+++ src/pigify.test.ts
@@ -0,0 +1,17 @@
+import { pigify } from "../src/pigify";
+
+describe("test pigify function", () => {
+  it('should return "" for "" input', () => {
+    expect(pigify("")).toBe("");
+  });
+
+  it('should return "igPay atinlay siay oolcay" for "Pig latin is cool" input', () => {
+    expect(pigify("Pig latin is cool")).toBe("igPay atinlay siay oolcay");
+  });
+
+  it('should return "elloHay orldway !" for "Hello world !" input', () => {
+    expect(pigify("Hello world !")).toBe("elloHay orldway !");
+  });
+
+
+});
